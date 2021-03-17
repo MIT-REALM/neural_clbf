5c0c7909ecaec745de19b0e5c7a3173a73c52817
Charles Dawson <cbd@mit.edu>
2021-03-17 18:57:23 -0400

    Delete qp-in-training code (still available in a side branch)

--- neural_clbf/controllers/__init__.py
+++ neural_clbf/controllers/__init__.py
@@ -1,5 +1,4 @@
 from .neural_rclbf_controller import NeuralrCLBFController
-from .neural_qp_rclbf_controller import NeuralQPrCLBFController
 from .utils import Controller
 
-__all__ = ["NeuralrCLBFController", "NeuralQPrCLBFController", "Controller"]
+__all__ = ["NeuralrCLBFController", "Controller"]
