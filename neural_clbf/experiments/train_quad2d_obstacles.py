d8e645b5b74a4e2d704d7f453f03412b4e0298d4
Charles Dawson <cbd@mit.edu>
2021-04-07 14:58:22 -0400

    debugging

--- neural_clbf/experiments/train_quad2d_obstacles.py
+++ neural_clbf/experiments/train_quad2d_obstacles.py
@@ -69,7 +69,7 @@ def main(args):
         trajectories_per_episode=500,
         trajectory_length=1000,
         val_split=0.1,
-        batch_size=1024,
+        batch_size=256,
     )
 
     # Define the scenarios
