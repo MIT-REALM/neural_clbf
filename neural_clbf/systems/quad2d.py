8a6dc4518ab5afc57a414743f698e925434b42b7
Charles Dawson <cbd@mit.edu>
2021-04-07 14:50:00 -0400

    debugging

--- neural_clbf/systems/quad2d.py
+++ neural_clbf/systems/quad2d.py
@@ -142,12 +142,8 @@ class Quad2D(ControlAffineSystem):
         limits for this system
         """
         # define upper and lower limits based around the nominal equilibrium input
-        upper_limit = self.nominal_params["m"] * grav / 2.0 + 0.0 * torch.tensor(
-            [20.0, 20.0]
-        )
-        lower_limit = self.nominal_params["m"] * grav / 2.0 - 0.0 * torch.tensor(
-            [20.0, 20.0]
-        )
+        upper_limit = self.nominal_params["m"] * grav / 2.0 + torch.tensor([1.0, 1.0])
+        lower_limit = self.nominal_params["m"] * grav / 2.0 - torch.tensor([1.0, 1.0])
 
         return (upper_limit, lower_limit)
 
