bd55744f4e3b5e3bc179652fb2a3a6c000fe5fe2
Charles Dawson <cbd@mit.edu>
2021-04-29 11:43:57 -0400

    Add inverted pendulum as a simple example

--- neural_clbf/experiments/train_inverted_pendulum.py
+++ neural_clbf/experiments/train_inverted_pendulum.py
@@ -6,7 +6,7 @@ import pytorch_lightning as pl
 from pytorch_lightning import loggers as pl_loggers
 import numpy as np
 
-from neural_clbf.controllers import NeuralSIDCLBFController
+from neural_clbf.controllers import NeuralCLBFController
 from neural_clbf.experiments.common.episodic_datamodule import (
     EpisodicDataModule,
 )
@@ -14,12 +14,12 @@ from neural_clbf.experiments.common.plotting import (
     plot_CLBF,
     rollout_CLBF,
 )
-from neural_clbf.systems import Quad2D
+from neural_clbf.systems import InvertedPendulum
 
 
 torch.multiprocessing.set_sharing_strategy("file_system")
 
-start_x = torch.tensor([[-0.75, 0.75, 0.0, 0.0, 0.0, 0.0]])
+start_x = torch.tensor([[0.5, -0.5]])
 controller_period = 0.01
 simulation_dt = 0.001
 
@@ -28,10 +28,10 @@ def rollout_plotting_cb(clbf_net):
     return rollout_CLBF(
         clbf_net,
         start_x=start_x,
-        plot_x_indices=[Quad2D.PX, Quad2D.PZ],
-        plot_x_labels=["$x$", "$z$"],
-        plot_u_indices=[Quad2D.U_RIGHT, Quad2D.U_LEFT],
-        plot_u_labels=["$u_r$", "$u_l$"],
+        plot_x_indices=[InvertedPendulum.THETA, InvertedPendulum.THETA_DOT],
+        plot_x_labels=["$\\theta$", "$\\dot{\\theta}$"],
+        plot_u_indices=[InvertedPendulum.U],
+        plot_u_labels=["$u$"],
         t_sim=6.0,
         n_sims_per_start=1,
         controller_period=controller_period,
@@ -45,47 +45,43 @@ def clbf_plotting_cb(clbf_net):
         clbf_net,
         domain=[(-1.0, 1.0), (-0.5, 1.0)],  # plot for x, z
         n_grid=15,
-        x_axis_index=Quad2D.PX,
-        y_axis_index=Quad2D.PZ,
-        x_axis_label="$x$",
-        y_axis_label="$z$",
+        x_axis_index=InvertedPendulum.THETA,
+        y_axis_index=InvertedPendulum.THETA_DOT,
+        x_axis_label="$\\theta$",
+        y_axis_label="$\\dot{\\theta}$",
     )
 
 
 def main(args):
     # Define the dynamics model
-    nominal_params = {"m": 1.0, "I": 0.01, "r": 0.25}
-    dynamics_model = Quad2D(
+    nominal_params = {"m": 1.0, "L": 1.0, "b": 0.1}
+    dynamics_model = InvertedPendulum(
         nominal_params, dt=simulation_dt, controller_dt=controller_period
     )
 
     # Initialize the DataModule
     initial_conditions = [
-        (-0.75, 0.75),  # x
-        (0.0, 1.0),  # z
-        (-np.pi / 4, np.pi / 4),  # theta
-        (-1.0, 1.0),  # vx
-        (-1.0, 1.0),  # vz
-        (-1.0, 1.0),  # theta_dot
+        (-np.pi / 4, np.pi / 4),  # x
+        (-1.0, 1.0),  # z
     ]
     data_module = EpisodicDataModule(
         dynamics_model,
         initial_conditions,
         trajectories_per_episode=1,
         trajectory_length=1,
-        fixed_samples=100000,
-        max_points=10000000,
+        fixed_samples=500000,
+        max_points=1000000,
         val_split=0.1,
-        batch_size=1024,
+        batch_size=64,
         safe_unsafe_goal_quotas=(0.2, 0.2, 0.2),
     )
 
     # Define the scenarios
     scenarios = [
         nominal_params,
-        {"m": 1.05, "I": 0.01, "r": 0.25},
-        {"m": 1.0, "I": 0.0105, "r": 0.25},
-        {"m": 1.05, "I": 0.0105, "r": 0.25},
+        {"m": 1.05, "L": 1.0, "b": 0.1},
+        {"m": 1.0, "L": 1.05, "b": 0.1},
+        {"m": 1.05, "L": 1.05, "b": 0.1},
     ]
 
     # Define the plotting callbacks
@@ -97,7 +93,7 @@ def main(args):
     ]
 
     # Initialize the controller
-    clbf_controller = NeuralSIDCLBFController(
+    clbf_controller = NeuralCLBFController(
         dynamics_model,
         scenarios,
         data_module,
@@ -106,8 +102,8 @@ def main(args):
         clbf_hidden_size=32,
         u_nn_hidden_layers=3,
         u_nn_hidden_size=32,
-        discrete_timestep=controller_period,
-        primal_learning_rate=1e-3,
+        controller_period=controller_period,
+        clbf_relaxation_penalty=50.0,
         epochs_per_episode=1000,
     )
     # Add the DataModule hooks
@@ -119,8 +115,8 @@ def main(args):
 
     # Initialize the logger and trainer
     tb_logger = pl_loggers.TensorBoardLogger(
-        "logs/quad2d_obstacles/",
-        name="comparison",
+        "logs/inverted_pendulum/",
+        name="qp_in_loop",
     )
     trainer = pl.Trainer.from_argparse_args(
         args, logger=tb_logger, reload_dataloaders_every_epoch=True
