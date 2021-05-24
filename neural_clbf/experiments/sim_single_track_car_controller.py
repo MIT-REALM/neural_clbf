318ad6e2210c3172c176b04bb1e7e517a6727fb9
Charles Dawson <cbd@mit.edu>
2021-05-24 13:13:30 -0400

    STCar experimentation

--- neural_clbf/experiments/sim_single_track_car_controller.py
+++ neural_clbf/experiments/sim_single_track_car_controller.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
 
 def doMain():
-    checkpoint = "logs/stcar_basic/v5.ckpt"
+    checkpoint = "logs/stcar_basic/v10.ckpt"
 
     controller_period = 0.01
     simulation_dt = 0.001
@@ -160,14 +160,14 @@ def single_rollout_straight_path(
     # (but first make somewhere to save the results)
     t_sim = 5.0
     n_sims = len(penalties)
-    num_timesteps = int(t_sim // simulation_dt)
+    T = int(t_sim // simulation_dt)
     start_x = torch.tensor(
         [[0.0, 1.0, 0.0, 1.0, -np.pi / 6, 0.0, 0.0]], device=clbf_controller.device
     )
-    x_sim = torch.zeros(
-        num_timesteps, n_sims, clbf_controller.dynamics_model.n_dims
-    ).type_as(start_x)
-    V_sim = torch.zeros(num_timesteps, n_sims, 1).type_as(start_x)
+    x_sim = torch.zeros(T, n_sims, clbf_controller.dynamics_model.n_dims).type_as(
+        start_x
+    )
+    V_sim = torch.zeros(T, n_sims, 1).type_as(start_x)
     for i in range(n_sims):
         x_sim[0, i, :] = start_x
         V_sim[0, i, 0] = clbf_controller.V(start_x)
@@ -175,11 +175,11 @@ def single_rollout_straight_path(
     x_nominal = torch.clone(x_sim)
     V_nominal = torch.clone(V_sim)
 
-    u_sim = torch.zeros(
-        num_timesteps, n_sims, clbf_controller.dynamics_model.n_controls
-    ).type_as(start_x)
+    u_sim = torch.zeros(T, n_sims, clbf_controller.dynamics_model.n_controls).type_as(
+        start_x
+    )
     controller_update_freq = int(controller_period / simulation_dt)
-    prog_bar_range = tqdm.trange(1, num_timesteps, desc="Straight Curve", leave=True)
+    prog_bar_range = tqdm.trange(1, T, desc="Straight Curve", leave=True)
     for tstep in prog_bar_range:
         # Get the current state
         x_current = x_sim[tstep - 1, :, :]
@@ -238,7 +238,7 @@ def single_rollout_straight_path(
     fig.set_size_inches(10, 6)
 
     # Get reference path
-    t = np.linspace(0, t_sim, num_timesteps)
+    t = np.linspace(0, t_sim, T)
     psi_ref = clbf_controller.dynamics_model.nominal_params["psi_ref"]
     x_ref = t * clbf_controller.dynamics_model.nominal_params["v_ref"] * np.cos(psi_ref)
     y_ref = t * clbf_controller.dynamics_model.nominal_params["v_ref"] * np.sin(psi_ref)
@@ -303,7 +303,7 @@ def single_rollout_straight_path(
     #     label="V",
     # )
     # # Plot markers indicating where the simulations were unsafe
-    # zeros = np.zeros((num_timesteps,))
+    # zeros = np.zeros((T,))
     # ax3.plot(
     #     t[:t_final],
     #     zeros[:t_final],
@@ -336,14 +336,14 @@ def single_rollout_circle_path(
     # (but first make somewhere to save the results)
     t_sim = 10.0
     n_sims = len(penalties)
-    num_timesteps = int(t_sim // simulation_dt)
+    T = int(t_sim // simulation_dt)
     start_x = 0.0 * torch.tensor(
         [[0.0, 1.0, 0.0, 1.0, -np.pi / 6, 0.0, 0.0]], device=clbf_controller.device
     )
-    x_sim = torch.zeros(
-        num_timesteps, n_sims, clbf_controller.dynamics_model.n_dims
-    ).type_as(start_x)
-    V_sim = torch.zeros(num_timesteps, n_sims, 1).type_as(start_x)
+    x_sim = torch.zeros(T, n_sims, clbf_controller.dynamics_model.n_dims).type_as(
+        start_x
+    )
+    V_sim = torch.zeros(T, n_sims, 1).type_as(start_x)
     for i in range(n_sims):
         x_sim[0, i, :] = start_x
         V_sim[0, i, 0] = clbf_controller.V(start_x)
@@ -354,16 +354,16 @@ def single_rollout_circle_path(
     # And create a place to store the reference path
     params = copy(clbf_controller.dynamics_model.nominal_params)
     params["omega_ref"] = 0.3
-    x_ref = np.zeros(num_timesteps)
-    y_ref = np.zeros(num_timesteps)
-    psi_ref = np.zeros(num_timesteps)
+    x_ref = np.zeros(T)
+    y_ref = np.zeros(T)
+    psi_ref = np.zeros(T)
     psi_ref[0] = 1.0
 
-    u_sim = torch.zeros(
-        num_timesteps, n_sims, clbf_controller.dynamics_model.n_controls
-    ).type_as(start_x)
+    u_sim = torch.zeros(T, n_sims, clbf_controller.dynamics_model.n_controls).type_as(
+        start_x
+    )
     controller_update_freq = int(controller_period / simulation_dt)
-    prog_bar_range = tqdm.trange(1, num_timesteps, desc="Circle Curve", leave=True)
+    prog_bar_range = tqdm.trange(1, T, desc="Circle Curve", leave=True)
     for tstep in prog_bar_range:
         # Get the path parameters at this point
         psi_ref[tstep] = simulation_dt * params["omega_ref"] + psi_ref[tstep - 1]
@@ -433,7 +433,7 @@ def single_rollout_circle_path(
     fig.set_size_inches(10, 12)
 
     # Get reference path
-    t = np.linspace(0, t_sim, num_timesteps)
+    t = np.linspace(0, t_sim, T)
     x_ref = np.tile(x_ref, (n_sims, 1)).T
     y_ref = np.tile(y_ref, (n_sims, 1)).T
     psi_ref = np.tile(psi_ref, (n_sims, 1)).T
@@ -496,7 +496,7 @@ def single_rollout_circle_path(
     #     label="V",
     # )
     # # Plot markers indicating where the simulations were unsafe
-    # zeros = np.zeros((num_timesteps,))
+    # zeros = np.zeros((T,))
     # ax3.plot(
     #     t[:t_final],
     #     zeros[:t_final],
@@ -524,36 +524,49 @@ def single_rollout_s_path(
 
     # Simulate!
     # (but first make somewhere to save the results)
-    t_sim = 5.0
+    t_sim = 5.0 / 5
     n_sims = 1
-    num_timesteps = int(t_sim // simulation_dt)
+    T = int(t_sim // simulation_dt)
     start_x = 0.0 * torch.tensor(
         [[0.0, 1.0, 0.0, 1.0, -np.pi / 6, 0.0, 0.0]], device=clbf_controller.device
     )
-    x_sim = torch.zeros(
-        num_timesteps, n_sims, clbf_controller.dynamics_model.n_dims
-    ).type_as(start_x)
-    V_sim = torch.zeros(num_timesteps, n_sims, 1).type_as(start_x)
+    x_sim = torch.zeros(T, n_sims, clbf_controller.dynamics_model.n_dims).type_as(
+        start_x
+    )
+    V_sim = torch.zeros(T, n_sims, 1).type_as(start_x)
     for i in range(n_sims):
         x_sim[0, i, :] = start_x
         V_sim[0, i, 0] = clbf_controller.V(start_x)
+    Vdot_sim = torch.zeros(T, n_sims, clbf_controller.n_scenarios + 1, 1).type_as(
+        start_x
+    )
 
+    u_sim = torch.zeros(T, n_sims, clbf_controller.dynamics_model.n_controls).type_as(
+        start_x
+    )
+
+    # Also create somewhere to save the simulations from the learned controller...
+    x_nn = torch.clone(x_sim)
+    V_nn = torch.clone(V_sim)
+    Vdot_nn = torch.clone(Vdot_sim)
+    u_nn = torch.clone(u_sim)
+
+    # And the nominal controller
     x_nominal = torch.clone(x_sim)
     V_nominal = torch.clone(V_sim)
+    Vdot_nominal = torch.clone(Vdot_sim)
+    u_nominal = torch.clone(u_sim)
 
     # And create a place to store the reference path
     params = copy(clbf_controller.dynamics_model.nominal_params)
     params["omega_ref"] = 0.3
-    x_ref = np.zeros(num_timesteps)
-    y_ref = np.zeros(num_timesteps)
-    psi_ref = np.zeros(num_timesteps)
+    x_ref = np.zeros(T)
+    y_ref = np.zeros(T)
+    psi_ref = np.zeros(T)
     psi_ref[0] = 1.0
 
-    u_sim = torch.zeros(
-        num_timesteps, n_sims, clbf_controller.dynamics_model.n_controls
-    ).type_as(start_x)
     controller_update_freq = int(controller_period / simulation_dt)
-    prog_bar_range = tqdm.trange(1, num_timesteps, desc="S-Curve", leave=True)
+    prog_bar_range = tqdm.trange(1, T, desc="S-Curve", leave=True)
     for tstep in prog_bar_range:
         # Get the path parameters at this point
         omega_ref_t = 1.5 * np.sign(np.sin(tstep * simulation_dt))
@@ -573,11 +586,31 @@ def single_rollout_s_path(
         if tstep == 1 or tstep % controller_update_freq == 0:
             u = clbf_controller(x_current)
             u_sim[tstep, :, :] = u
+
+            # Also predict the difference in V from linearization
+            # ... in each scenario
+            Lf_V, Lg_V = clbf_controller.V_lie_derivatives(x_current)
+            for i in range(clbf_controller.n_scenarios):
+                Vdot = Lf_V[:, i, :].unsqueeze(1) + torch.bmm(
+                    Lg_V[:, i, :].unsqueeze(1), u.unsqueeze(-1)
+                )
+                Vdot = Vdot.reshape(-1, 1)
+                Vdot_sim[tstep, :, i, 0] = Vdot
+            # and with the true parameters
+            Lf_V, Lg_V = clbf_controller.V_lie_derivatives(x_current, [pt])
+            Vdot = Lf_V[:, 0, :].unsqueeze(1) + torch.bmm(
+                Lg_V[:, 0, :].unsqueeze(1), u.unsqueeze(-1)
+            )
+            Vdot = Vdot.reshape(-1, 1)
+            Vdot_sim[tstep, :, -1, 0] = Vdot
         else:
             u = u_sim[tstep - 1, :, :]
             u_sim[tstep, :, :] = u
 
-        # Simulate forward using the dynamics
+            # Copy the estimate of Vdot
+            Vdot = Vdot_sim[tstep - 1, :, :, :]
+            Vdot_sim[tstep, :, :, :] = Vdot
+
         for i in range(n_sims):
             xdot = clbf_controller.dynamics_model.closed_loop_dynamics(
                 x_current[i, :].unsqueeze(0),
@@ -589,22 +622,87 @@ def single_rollout_s_path(
         # Get the CLBF values
         V_sim[tstep, :, 0] = clbf_controller.V(x_current).squeeze()
 
+        # and repeat for the NN controller
+        # Get the current state
+        x_current = x_nn[tstep - 1, :, :]
+        # Get the control input at the current state if it's time
+        if tstep == 1 or tstep % controller_update_freq == 0:
+            u = clbf_controller.u(x_current)
+            u_nn[tstep, :, :] = u
+
+            # Also predict the difference in V from linearization
+            # ... in each scenario
+            Lf_V, Lg_V = clbf_controller.V_lie_derivatives(x_current)
+            for i in range(clbf_controller.n_scenarios):
+                Vdot = Lf_V[:, i, :].unsqueeze(1) + torch.bmm(
+                    Lg_V[:, i, :].unsqueeze(1), u.unsqueeze(-1)
+                )
+                Vdot = Vdot.reshape(-1, 1)
+                Vdot_nn[tstep, :, i, 0] = Vdot
+            # and with the true parameters
+            Lf_V, Lg_V = clbf_controller.V_lie_derivatives(x_current, [pt])
+            Vdot = Lf_V[:, 0, :].unsqueeze(1) + torch.bmm(
+                Lg_V[:, 0, :].unsqueeze(1), u.unsqueeze(-1)
+            )
+            Vdot = Vdot.reshape(-1, 1)
+            Vdot_nn[tstep, :, -1, 0] = Vdot
+        else:
+            u = u_nn[tstep - 1, :, :]
+            u_nn[tstep, :, :] = u
+
+            # Copy the estimate of Vdot
+            Vdot = Vdot_nn[tstep - 1, :, :, :]
+            Vdot_nn[tstep, :, :, :] = Vdot
+
+        # Simulate forward using the dynamics
+        for i in range(n_sims):
+            xdot = clbf_controller.dynamics_model.closed_loop_dynamics(
+                x_current[i, :].unsqueeze(0),
+                u_nn[tstep, i, :].unsqueeze(0),
+                pt,
+            )
+            x_nn[tstep, i, :] = x_current[i, :] + simulation_dt * xdot.squeeze()
+
+        # Get the CLBF values
+        V_nn[tstep, :, 0] = clbf_controller.V(x_current).squeeze()
+
         # and repeat for the nominal controller
         # Get the current state
         x_current = x_nominal[tstep - 1, :, :]
         # Get the control input at the current state if it's time
         if tstep == 1 or tstep % controller_update_freq == 0:
             u = clbf_controller.dynamics_model.u_nominal(x_current, pt)
-            u_sim[tstep, :, :] = u
+            u_nominal[tstep, :, :] = u
+
+            # Also predict the difference in V from linearization
+            # ... in each scenario
+            Lf_V, Lg_V = clbf_controller.V_lie_derivatives(x_current)
+            for i in range(clbf_controller.n_scenarios):
+                Vdot = Lf_V[:, i, :].unsqueeze(1) + torch.bmm(
+                    Lg_V[:, i, :].unsqueeze(1), u.unsqueeze(-1)
+                )
+                Vdot = Vdot.reshape(-1, 1)
+                Vdot_nominal[tstep, :, i, 0] = Vdot
+            # and with the true parameters
+            Lf_V, Lg_V = clbf_controller.V_lie_derivatives(x_current, [pt])
+            Vdot = Lf_V[:, 0, :].unsqueeze(1) + torch.bmm(
+                Lg_V[:, 0, :].unsqueeze(1), u.unsqueeze(-1)
+            )
+            Vdot = Vdot.reshape(-1, 1)
+            Vdot_nominal[tstep, :, -1, 0] = Vdot
         else:
-            u = u_sim[tstep - 1, :, :]
-            u_sim[tstep, :, :] = u
+            u = u_nominal[tstep - 1, :, :]
+            u_nominal[tstep, :, :] = u
+
+            # Copy the estimate of Vdot
+            Vdot = Vdot_nominal[tstep - 1, :, :, :]
+            Vdot_nominal[tstep, :, :, :] = Vdot
 
         # Simulate forward using the dynamics
         for i in range(n_sims):
             xdot = clbf_controller.dynamics_model.closed_loop_dynamics(
                 x_current[i, :].unsqueeze(0),
-                u_sim[tstep, i, :].unsqueeze(0),
+                u_nominal[tstep, i, :].unsqueeze(0),
                 pt,
             )
             x_nominal[tstep, i, :] = x_current[i, :] + simulation_dt * xdot.squeeze()
@@ -615,11 +713,12 @@ def single_rollout_s_path(
         t_final = tstep
 
     # Plot!
-    fig, axs = plt.subplots(3, 1)
-    fig.set_size_inches(10, 12)
+    fig = plt.figure()
+    gs = fig.add_gridspec(3, 3)
+    fig.set_size_inches(12, 12)
 
     # Get reference path
-    t = np.linspace(0, t_sim, num_timesteps)
+    t = np.linspace(0, t_sim, T)
     x_ref = np.tile(x_ref, (n_sims, 1)).T
     y_ref = np.tile(y_ref, (n_sims, 1)).T
     psi_ref = np.tile(psi_ref, (n_sims, 1)).T
@@ -630,29 +729,44 @@ def single_rollout_s_path(
     x_world = x_ref + x_err_path * np.cos(psi_ref) - y_err_path * np.sin(psi_ref)
     y_world = y_ref + x_err_path * np.sin(psi_ref) + y_err_path * np.cos(psi_ref)
 
+    x_err_nn = x_nn[:, :, clbf_controller.dynamics_model.SXE].cpu().numpy()
+    y_err_nn = x_nn[:, :, clbf_controller.dynamics_model.SYE].cpu().numpy()
+    x_world_nn = x_ref + x_err_nn * np.cos(psi_ref) - y_err_nn * np.sin(psi_ref)
+    y_world_nn = y_ref + x_err_nn * np.sin(psi_ref) + y_err_nn * np.cos(psi_ref)
+
     x_err_nom = x_nominal[:, :, clbf_controller.dynamics_model.SXE].cpu().numpy()
     y_err_nom = x_nominal[:, :, clbf_controller.dynamics_model.SYE].cpu().numpy()
     x_world_nom = x_ref + x_err_nom * np.cos(psi_ref) - y_err_nom * np.sin(psi_ref)
     y_world_nom = y_ref + x_err_nom * np.sin(psi_ref) + y_err_nom * np.cos(psi_ref)
 
-    ax1 = axs[0]
-    ax1.plot([], [], linestyle="-", label="CLBF Tracking")
+    ax1 = fig.add_subplot(gs[:, 0])
     ax1.plot(
-        x_world[:t_final],
-        y_world[:t_final],
-        linestyle="-",
+        x_world[:t_final, 0],
+        y_world[:t_final, 0],
+        linestyle="solid",
+        label="CLBF",
+        color="red",
+    )
+    ax1.plot(
+        x_world_nn[:t_final, 0],
+        y_world_nn[:t_final, 0],
+        linestyle="dashdot",
+        label="NN",
+        color="blue",
     )
     ax1.plot(
         x_world_nom[:t_final, 0],
         y_world_nom[:t_final, 0],
-        linestyle="-.",
-        label="Nominal Tracking",
+        linestyle="dashed",
+        label="Nominal",
+        color="green",
     )
     ax1.plot(
         x_ref[:t_final, 0],
         y_ref[:t_final, 0],
-        linestyle=":",
-        label="Reference",
+        linestyle="dotted",
+        label="Ref",
+        color="black",
     )
     ax1.set_xlabel("$x$")
     ax1.set_ylabel("$y$")
@@ -661,38 +775,142 @@ def single_rollout_s_path(
     ax1.set_xlim([np.min(x_ref) - 3, np.max(x_ref) + 3])
     ax1.set_aspect("equal")
 
-    ax2 = axs[1]
-    ax2.plot([], [], linestyle="-", label="CLBF-QP")
-    ax2.plot([], [], linestyle=":", label="Nominal")
-    for i in range(n_sims):
-        ax2.plot(
-            t[:t_final],
-            x_sim[:t_final, i, :].norm(dim=-1).squeeze().cpu().numpy(),
-            linestyle="-",
-        )
-    for i in range(n_sims):
-        ax2.plot(
-            t[:t_final],
-            x_nominal[:t_final, i, :].norm(dim=-1).squeeze().cpu().numpy(),
-            linestyle=":",
-        )
-        break
+    ax2 = fig.add_subplot(gs[0, 1:])
+    ax2.plot(
+        t[:t_final],
+        x_sim[:t_final, 0, :].norm(dim=-1).squeeze().cpu().numpy(),
+        linestyle="-",
+        label="CLBF",
+        color="red",
+    )
+    ax2.plot(
+        t[:t_final],
+        x_nn[:t_final, 0, :].norm(dim=-1).squeeze().cpu().numpy(),
+        linestyle=":",
+        label="NN",
+        color="blue",
+    )
+    ax2.plot(
+        t[:t_final],
+        x_nominal[:t_final, 0, :].norm(dim=-1).squeeze().cpu().numpy(),
+        linestyle=":",
+        label="Nominal",
+        color="green",
+    )
 
     ax2.legend()
     ax2.set_xlabel("$t$")
+    ax2.set_ylabel("Tracking error")
 
-    ax3 = axs[2]
+    ax3 = fig.add_subplot(gs[1, 1:])
     ax3.plot(
         t[:t_final],
         V_sim[:t_final, :, :].squeeze().cpu().numpy(),
-        label="V",
+        label="CLBF",
+        linestyle="solid",
+        color="red",
+    )
+    ax3.plot(
+        t[:t_final],
+        V_nn[:t_final, :, :].squeeze().cpu().numpy(),
+        label="NN",
+        linestyle="solid",
+        color="blue",
+    )
+    ax3.plot(
+        t[:t_final],
+        V_nominal[:t_final, :, :].squeeze().cpu().numpy(),
+        label="Nominal",
+        linestyle="solid",
+        color="green",
     )
     # Plot markers indicating where the simulations were unsafe
-    zeros = np.zeros((num_timesteps,))
+    zeros = np.zeros((T,))
     ax3.plot(
         t[:t_final],
         zeros[:t_final],
+        linestyle="dotted",
+        color="black",
     )
+    ax3.legend()
+    ax3.set_xlabel("$t$")
+    ax3.set_ylabel("$V$")
+
+    ax4 = fig.add_subplot(gs[2, 1:])
+    Vdot_actual_sim = torch.diff(V_sim, dim=0) / simulation_dt
+    Vdot_actual_nn = torch.diff(V_nn, dim=0) / simulation_dt
+    Vdot_actual_nominal = torch.diff(V_nominal, dim=0) / simulation_dt
+    ax4.plot(
+        t[1:t_final],
+        Vdot_sim[1:t_final, :, -1, :].squeeze().cpu().numpy(),
+        linestyle="solid",
+        color="red",
+    )
+    ax4.fill_between(
+        t[1:t_final],
+        Vdot_sim[1:t_final, :, 0, :].squeeze().cpu().numpy(),
+        Vdot_sim[1:t_final, :, 1, :].squeeze().cpu().numpy(),
+        alpha=0.1,
+        color="red",
+    )
+    ax4.plot(
+        t[1:t_final],
+        Vdot_actual_sim[1:t_final, :, :].squeeze().cpu().numpy(),
+        linestyle="dotted",
+        color="red",
+    )
+    ax4.plot(
+        t[1:t_final],
+        Vdot_nn[1:t_final, :, -1, :].squeeze().cpu().numpy(),
+        linestyle="solid",
+        color="blue",
+    )
+    ax4.fill_between(
+        t[1:t_final],
+        Vdot_nn[1:t_final, :, 0, :].squeeze().cpu().numpy(),
+        Vdot_nn[1:t_final, :, 1, :].squeeze().cpu().numpy(),
+        alpha=0.1,
+        color="blue",
+    )
+    ax4.plot(
+        t[1:t_final],
+        Vdot_actual_nn[1:t_final, :, :].squeeze().cpu().numpy(),
+        linestyle="dotted",
+        color="blue",
+    )
+    ax4.plot(
+        t[1:t_final],
+        Vdot_nominal[1:t_final, :, -1, :].squeeze().cpu().numpy(),
+        label="Linearized (true parameters)",
+        linestyle="dashed",
+        color="green",
+    )
+    ax4.fill_between(
+        t[1:t_final],
+        Vdot_nominal[1:t_final, :, 0, :].squeeze().cpu().numpy(),
+        Vdot_nominal[1:t_final, :, 1, :].squeeze().cpu().numpy(),
+        label="Linearized (scenarios)",
+        alpha=0.1,
+        color="green",
+    )
+    ax4.plot(
+        t[1:t_final],
+        Vdot_actual_nominal[1:t_final, :, :].squeeze().cpu().numpy(),
+        label="Simulated",
+        linestyle="solid",
+        color="green",
+    )
+    # Plot markers indicating where the simulations were unsafe
+    zeros = np.zeros((T,))
+    ax4.plot(
+        t[:t_final],
+        zeros[:t_final],
+        linestyle="dotted",
+        color="black",
+    )
+    ax4.legend()
+    ax4.set_xlabel("$t$")
+    ax4.set_ylabel("$dV/dt$")
 
     fig.tight_layout()
 
