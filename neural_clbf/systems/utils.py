9130f39ba985009756196cb4562fbd6a30ce3e15
Charles Dawson <cbd@mit.edu>
2021-06-07 17:06:58 -0400

    Add robust linear quadratic Lyapunov function calculation

--- neural_clbf/systems/utils.py
+++ neural_clbf/systems/utils.py
@@ -3,6 +3,7 @@ from typing import Dict, List
 
 import numpy as np
 import scipy.linalg
+import cvxpy as cp
 
 
 # Gravitation acceleration
@@ -67,3 +68,35 @@ def discrete_lyap(Acl: np.ndarray, Q: np.ndarray):
     """
     P = scipy.linalg.solve_discrete_lyapunov(Acl.T, Q)
     return P
+
+
+def robust_continuous_lyap(Acl_list: List[np.ndarray], Q: np.ndarray):
+    """Solve the continuous time lyapunov equation robustly. That is, find P such that
+
+    Acl.T P + P Acl <= -Q
+
+    for each A
+    """
+    # Sanity check the provided scenarios. They should all have the same dimension
+    # and they should all be stable
+    n_dims = Q.shape[0]
+    for Acl in Acl_list:
+        assert Acl.shape == Q.shape, "Acl shape should be consistent with Q"
+        assert (np.linalg.eigvals(Acl) < 0).all(), "Acl should be stable"
+
+    # We'll find P using a semidefinite program. First we need a matrix variable for P
+    P = cp.Variable((n_dims, n_dims), symmetric=True)
+
+    # Each scenario implies a semidefiniteness constraint
+    constraints = [P >> 0.1 * Q]  # P must itself be semidefinite
+    for Acl in Acl_list:
+        constraints.append(Acl.T @ P + P @ Acl << -P)
+
+    # The objective is to minimize the size of the elements of P
+    objective = cp.trace(np.ones((n_dims, n_dims)) @ P)
+
+    # Solve!
+    prob = cp.Problem(cp.Minimize(objective), constraints)
+    prob.solve()
+
+    return P.value
