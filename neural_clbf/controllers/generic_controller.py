d4748424f8513037bd99a86eee285c72fab24f97
Charles Dawson <cbd@mit.edu>
2021-06-15 17:13:05 -0400

    Refactor to move sim and training back into one directory, since the structure needs to be the same as when the checkpoints were saved (blech; I'll fix that later). Also add some RMPC stuff from Zengyi and an example script for saving rollouts

--- neural_clbf/controllers/generic_controller.py
+++ neural_clbf/controllers/generic_controller.py
@@ -14,7 +14,3 @@ class GenericController(ABC):
         super(GenericController, self).__init__()
         self.controller_period = controller_period
         self.dynamics_model = dynamics_model
-
-    def __call__(self, x):
-        """Compute and return the control input for a tensor of states x"""
-        raise NotImplementedError
