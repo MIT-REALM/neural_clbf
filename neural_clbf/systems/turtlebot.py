ec1f68fc3548b51b07dca7067bacf2d99529e940
Bethany <bethlow@mit.edu>
2021-07-07 14:41:12 -0400

    Working training with updated control limits and nominal control

--- neural_clbf/systems/turtlebot.py
+++ neural_clbf/systems/turtlebot.py
@@ -52,7 +52,8 @@ class TurtleBot(ControlAffineSystem):
             ValueError if nominal_params are not valid for this system
         """
         super().__init__(
-            nominal_params, dt=dt, controller_dt=controller_dt, scenarios=scenarios
+            nominal_params, dt=dt, controller_dt=controller_dt, scenarios=scenarios,
+            use_linearized_controller=False
         )
 
     def validate_params(self, params: Scenario) -> bool:
@@ -111,8 +112,10 @@ class TurtleBot(ControlAffineSystem):
         # define upper and lower limits based around the nominal equilibrium input
         # TODO @bethlow these are relaxed for now, but eventually
         # these values should be measured on the hardware.
-        upper_limit = torch.tensor([100 * 10.0])
-        lower_limit = -torch.tensor([100 * 10.0])
+        upper_limit = torch.ones(self.n_controls)
+        upper_limit[TurtleBot.V] = 100 * 10.0
+        upper_limit[TurtleBot.THETA_DOT] = 2.0 * np.pi
+        lower_limit = -1.0 * upper_limit
 
         return (upper_limit, lower_limit)
 
@@ -222,3 +225,28 @@ class TurtleBot(ControlAffineSystem):
         g = g.matmul(v)
 
         return g
+
+    def u_nominal(self, x: torch.Tensor) -> torch.Tensor:
+        
+
+        self.P = torch.eye(3,3)
+        self.K = torch.ones(self.n_controls, self.n_dims)
+
+        K = self.K.type_as(x)
+        goal = self.goal_point.squeeze().type_as(x)
+        u_nominal = -(K @ (x - goal).T).T
+
+        # Adjust for the equilibrium setpoint
+        u = u_nominal + self.u_eq.type_as(x)
+        # Clamp given the control limits
+        # import pdb; pdb.set_trace()
+        upper_u_lim, lower_u_lim = self.control_limits
+        u = torch.clamp(u, min=lower_u_lim[0].item(), max=upper_u_lim[0].item())
+        # for dim_idx in range(self.n_controls):
+        #     u[:, dim_idx] = torch.clamp(
+        #         u[:, dim_idx],
+        #         min=lower_u_lim[dim_idx].item(),
+        #         max=upper_u_lim[dim_idx].item(),
+        #     ) 
+
+        return u
