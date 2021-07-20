3e373399263a9e3eeafed91810c3d9678d258716
Charles Dawson <cbd@mit.edu>
2021-07-20 10:28:58 -0400

    Add some computationally intractable verification strategies

--- neural_clbf/evaluation/eval_linear_satellite.py
+++ neural_clbf/evaluation/eval_linear_satellite.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 from neural_clbf.controllers import NeuralCBFController
+from neural_clbf.experiments import CLFVerificationExperiment
 
 
 def eval_linear_satellite():
@@ -33,6 +34,31 @@ def plot_linear_satellite():
     )
 
 
+def verify_linear_satellite():
+    # Load the checkpoint file. This should include the experiment suite used during
+    # training.
+    log_dir = "saved_models/aas/linear_satellite_cbf/commit_318eb38/"
+    neural_controller = NeuralCBFController.load_from_checkpoint(log_dir + "v0.ckpt")
+
+    # Define an experiment to validate the CBF
+    domain = [
+        (-2.5, 2.5),
+        (-2.5, 2.5),
+        (-2.5, 2.5),
+        (-1.0, 1.0),
+        (-1.0, 1.0),
+        (-1.0, 1.0),
+    ]
+    n_grid = 10
+    verification_experiment = CLFVerificationExperiment(
+        "CLF_Verification", domain, n_grid=n_grid
+    )
+
+    # Run the experiments and save the results
+    verification_experiment.run_and_plot(neural_controller, display_plots=True)
+
+
 if __name__ == "__main__":
     # eval_linear_satellite()
-    plot_linear_satellite()
+    # plot_linear_satellite()
+    verify_linear_satellite()
