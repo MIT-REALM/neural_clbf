e076214efaddebedf5d5e920da2fabd9068dd964
Charles Dawson <cbd@mit.edu>
2021-07-28 10:44:04 -0400

    Increase controller period

--- neural_clbf/training/train_turtlebot_lidar.py
+++ neural_clbf/training/train_turtlebot_lidar.py
@@ -23,7 +23,7 @@ from neural_clbf.training.utils import current_git_hash
 torch.multiprocessing.set_sharing_strategy("file_system")
 
 batch_size = 64
-controller_period = 0.01
+controller_period = 0.05
 
 start_x = torch.tensor(
     [
