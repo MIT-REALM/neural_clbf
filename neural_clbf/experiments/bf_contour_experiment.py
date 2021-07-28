446f1ad21f35c332531c38dd57709223363975e2
Charles Dawson <cbd@mit.edu>
2021-07-28 18:36:07 -0400

    Use contourf instead of scatter plot for the contour (reverts debugging change)

--- neural_clbf/experiments/bf_contour_experiment.py
+++ neural_clbf/experiments/bf_contour_experiment.py
@@ -167,12 +167,10 @@ class BFContourExperiment(Experiment):
         sns.set_theme(context="talk", style="white")
 
         # Plot a contour of h
-        fig = plt.figure()
-        # fig, ax = plt.subplots(projection="3d")
-        ax = fig.add_subplot(projection="3d")
+        fig, ax = plt.subplots(1, 1)
         fig.set_size_inches(12, 8)
 
-        contours = ax.scatter(
+        contours = ax.tricontourf(
             results_df[self.x_axis_label],
             results_df[self.y_axis_label],
             results_df["h"],
@@ -180,46 +178,46 @@ class BFContourExperiment(Experiment):
         )
         plt.colorbar(contours, ax=ax, orientation="vertical")
 
-        # # Overlay the safe/unsafe regions (if specified)
-        # if self.plot_unsafe_region:
-        #     ax.plot([], [], c="green", label="Safe Region")
-        #     ax.tricontour(
-        #         results_df[self.x_axis_label],
-        #         results_df[self.y_axis_label],
-        #         results_df["Safe region"],
-        #         colors=["green"],
-        #         levels=[0.5],
-        #     )
-        #     ax.plot([], [], c="magenta", label="Unsafe Region")
-        #     ax.tricontour(
-        #         results_df[self.x_axis_label],
-        #         results_df[self.y_axis_label],
-        #         results_df["Unsafe region"],
-        #         colors=["magenta"],
-        #         levels=[0.5],
-        #     )
+        # Overlay the safe/unsafe regions (if specified)
+        if self.plot_unsafe_region:
+            ax.plot([], [], c="green", label="Safe Region")
+            ax.tricontour(
+                results_df[self.x_axis_label],
+                results_df[self.y_axis_label],
+                results_df["Safe region"],
+                colors=["green"],
+                levels=[0.5],
+            )
+            ax.plot([], [], c="magenta", label="Unsafe Region")
+            ax.tricontour(
+                results_df[self.x_axis_label],
+                results_df[self.y_axis_label],
+                results_df["Unsafe region"],
+                colors=["magenta"],
+                levels=[0.5],
+            )
 
-        #     # Plot the environment if possible
-        #     if hasattr(controller_under_test.dynamics_model, "plot_environment"):
-        #         controller_under_test.dynamics_model.plot_environment(ax)
+            # Plot the environment if possible
+            if hasattr(controller_under_test.dynamics_model, "plot_environment"):
+                controller_under_test.dynamics_model.plot_environment(ax)
 
-        #     ax.plot([], [], c="blue", label="h(o(x)) = 0.0")
-        #     if hasattr(controller_under_test, "safe_level"):
-        #         ax.tricontour(
-        #             results_df[self.x_axis_label],
-        #             results_df[self.y_axis_label],
-        #             results_df["h"],
-        #             colors=["blue"],
-        #             levels=[0.0],
-        #         )
-        #     else:
-        #         ax.tricontour(
-        #             results_df[self.x_axis_label],
-        #             results_df[self.y_axis_label],
-        #             results_df["h"],
-        #             colors=["blue"],
-        #             levels=[0.0],
-        #         )
+            ax.plot([], [], c="blue", label="h(o(x)) = 0.0")
+            if hasattr(controller_under_test, "safe_level"):
+                ax.tricontour(
+                    results_df[self.x_axis_label],
+                    results_df[self.y_axis_label],
+                    results_df["h"],
+                    colors=["blue"],
+                    levels=[0.0],
+                )
+            else:
+                ax.tricontour(
+                    results_df[self.x_axis_label],
+                    results_df[self.y_axis_label],
+                    results_df["h"],
+                    colors=["blue"],
+                    levels=[0.0],
+                )
 
         # Make the legend
         ax.legend(
