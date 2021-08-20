cd82f9fece99fa8c754c233c4213b453767be5e7
SpacePanda-12 <86313182+SpacePanda-12@users.noreply.github.com>
2021-08-20 11:12:04 -0400

    Turtlebot-clbf interface now functioning
    Updated code to make it such that the turtlebots can now be controlled using the neural_clbf code base

--- neural_clbf/systems/turtlebot.py
+++ neural_clbf/systems/turtlebot.py
@@ -30,6 +30,7 @@ class TurtleBot(ControlAffineSystem):
     X = 0
     Y = 1
     THETA = 2
+    
     # Control indices
     V = 0
     THETA_DOT = 1
@@ -98,8 +99,8 @@ class TurtleBot(ControlAffineSystem):
         """
         # define upper and lower limits based around the nominal equilibrium input
         upper_limit = torch.ones(self.n_dims)
-        upper_limit[TurtleBot.X] = 2.0
-        upper_limit[TurtleBot.Y] = 2.0
+        upper_limit[TurtleBot.X] = 5.0
+        upper_limit[TurtleBot.Y] = 5.0
         upper_limit[TurtleBot.THETA] = np.pi
 
         lower_limit = -1.0 * upper_limit
@@ -113,11 +114,9 @@ class TurtleBot(ControlAffineSystem):
         limits for this system
         """
         # define upper and lower limits based around the nominal equilibrium input
-        # TODO @bethlow these are relaxed for now, but eventually
-        # these values should be measured on the hardware.
         upper_limit = torch.ones(self.n_controls)
-        upper_limit[TurtleBot.V] = 100 * 10.0
-        upper_limit[TurtleBot.THETA_DOT] = 4.0 * np.pi
+        upper_limit[TurtleBot.V] = 3
+        upper_limit[TurtleBot.THETA_DOT] = 3.0 * np.pi
         lower_limit = -1.0 * upper_limit
 
         return (upper_limit, lower_limit)
@@ -127,6 +126,7 @@ class TurtleBot(ControlAffineSystem):
         args:
             x: a tensor of points in the state space
         """
+        x = x[:][0:2]
         safe_mask = x.norm(dim=-1) <= 0.8
 
         return safe_mask
@@ -136,6 +136,7 @@ class TurtleBot(ControlAffineSystem):
         args:
             x: a tensor of points in the state space
         """
+        x = x[:][0:2]
         unsafe_mask = x.norm(dim=-1) >= 1.5
 
         return unsafe_mask
@@ -145,6 +146,7 @@ class TurtleBot(ControlAffineSystem):
         args:
             x: a tensor of points in the state space
         """
+        x = x[:][0:2]
         goal_mask = x.norm(dim=-1) <= 0.3
 
         return goal_mask
@@ -219,30 +221,64 @@ class TurtleBot(ControlAffineSystem):
 
         return g
 
-    def u_nominal(self, x: torch.Tensor) -> torch.Tensor:
+
+    def u_nominal(
+        self, x: torch.Tensor, params: Optional[Scenario] = None
+    ) -> torch.Tensor:
         """
-        Return u_nominal using feedback law, overriding the typical LQR approximation
-        due to nonlinear turtlebot system
+        Compute the nominal control for the nominal parameters.
         args:
             x: bs x self.n_dims tensor of state
+            params: the model parameters used
         returns:
-            u_nominal: bs x self.n_controls tensor of the nominal controls
-
+            u_nominal: bs x self.n_controls tensor of controls
         """
-        # Compute nominal control from feedback and equilibrium control
-        # v = -(x + y + theta)
-        # omega = -(x + y theta)
-        self.P = torch.eye(3, 3)
-        self.K = torch.ones(self.n_controls, self.n_dims)
+        # The turtlebot linearization is not well-behaved, so we create our own
+        # P and K matrices (mainly as placeholders)
+        self.P = torch.eye(self.n_dims)
+        self.K = torch.zeros(self.n_controls, self.n_dims)
 
-        K = self.K.type_as(x)
-        goal = self.goal_point.squeeze().type_as(x)
-        u_nominal = -(K @ (x - goal).T).T
+        # This controller should navigate us towards the origin. We can do this by
+        # setting a velocity proportional to the inner product of the vector
+        # from the turtlebot to the origin and the vector pointing out in front of
+        # the turtlebot. If the bot is pointing away from the origin, this inner product
+        # will be negative, so we'll drive backwards towards the goal. If the bot
+        # is pointing towards the origin, it will drive forwards.
+        u = torch.zeros(x.shape[0], self.n_controls)
 
-        # Adjust for the equilibrium setpoint
-        u = u_nominal + self.u_eq.type_as(x)
+        v_scaling = 1.0
+        bot_to_origin = -x[:, : TurtleBot.Y + 1].reshape(-1, 1, 2)
+        theta = x[:, TurtleBot.THETA]
+        bot_facing = torch.stack((torch.cos(theta), torch.sin(theta))).T.unsqueeze(-1)
+        u[:, TurtleBot.V] = v_scaling * torch.bmm(bot_to_origin, bot_facing).squeeze()
+
+        # In addition to setting the velocity towards the origin, we also need to steer
+        # towards the origin. We can do this via P control on the angle between the
+        # turtlebot and the vector to the origin.
+        #
+        # However, this angle becomes ill-defined as the bot approaches the origin, so
+        # so we switch this term off if the bot is too close (and instead just control
+        # theta to zero)
+        phi_control_on = bot_to_origin.norm(dim=-1) >= 0.2
+        phi_control_on = phi_control_on.reshape(-1)
+        omega_scaling = 5.0
+        angle_from_origin_to_bot = torch.atan2(x[:, TurtleBot.Y], x[:, TurtleBot.X])
+        phi = theta - angle_from_origin_to_bot
+        # First, wrap the angle error into [-pi, pi]
+        phi = torch.atan2(torch.sin(phi), torch.cos(phi))
+        # Now decrement any errors > pi/2 by pi and increment any errors < -pi / 2 by pi
+        # Then P controlling the error to zero will drive the bot to point towards the
+        # origin
+        phi[phi > np.pi / 2.0] -= np.pi
+        phi[phi < -np.pi / 2.0] += np.pi
+
+        # Only apply this P control when the bot is far enough from the origin;
+        # default to P control on theta
+        u[:, TurtleBot.THETA_DOT] = -omega_scaling * theta
+        u[phi_control_on, TurtleBot.THETA_DOT] = -omega_scaling * phi[phi_control_on]
+
         # Clamp given the control limits
-        upper_u_lim, lower_u_lim = self.control_limits
-        u = torch.clamp(u,upper_u_lim, lower_u_lim)
+        u_upper, u_lower = self.control_limits
+        u = torch.clamp(u, u_lower, u_upper)
 
         return u
