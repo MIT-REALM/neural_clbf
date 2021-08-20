1869e3748552578de35cec56cc27463768eb4488
SpacePanda-12 <86313182+SpacePanda-12@users.noreply.github.com>
2021-08-20 11:23:54 -0400

    Turtlebot interface now functioning
    Updated code such that the turtlebots can now be controlled with the code base

---
+++ turtlebot_integration/turtlebot_scripts/send_command.py
@@ -0,0 +1,61 @@
+import rospy
+from .battery_status import batteryLevel
+from .message_creator import create_message
+from math import pi
+
+
+def execute_command(command_publisher, move_command, move_command_translational,
+                    move_command_angular, position, rotation):
+    """
+    
+    Sends movement command created by a control script to a turtlebot.
+    Also pushes information to message_creator.py to output a message
+    to the console.
+
+    Arguments:
+        command_publisher: publisher node on which to send commands
+
+        move_command: empty twist object; not a command in and of itself.
+        the angular and linear properties of move_command are set to
+        move_command_angular and move_command_translational, respectively,
+        within this function.
+
+        move_command_translational: command to make the turtlebot
+        move either forward or backwards
+
+        move_command_angular: command to make the turtlebot rotate
+
+        position: turtlebot's current position
+
+        rotation: turtlebot's current attitude
+    
+    """
+    # TODO call limits from turtlebot system file instead of hard coding here
+    # max_command_translational = 0.05
+    # max_command_rotational = 0.1
+
+    # if move_command_translational > max_command_translational:
+    #     move_command_translational = max_command_translational
+    # elif move_command_translational < -max_command_translational:
+    #     move_command_translational = -max_command_translational
+
+    move_command.linear.x = move_command_translational
+
+    # if move_command_angular > max_command_rotational:
+    #     move_command_angular = max_command_rotational
+    # elif move_command_angular < -max_command_rotational:
+    #     move_command_angular = -max_command_rotational
+
+    move_command.angular.z = move_command_angular
+
+    # send the command to the turtlebot
+    command_publisher.publish(move_command)
+    
+    # publish turtlebot status to console
+    create_message(
+        move_command, batteryLevel, position, rotation*360/(2*pi)
+    )
+
+    # briefly pause to avoid overloading turtlebot with commands, which will
+    # cause sync issues
+    rospy.sleep(.001)
