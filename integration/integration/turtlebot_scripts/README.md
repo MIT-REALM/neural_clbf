9369a4abb1ca5499aa5ff9b2670477a5d97c149c
SpacePanda-12 <86313182+SpacePanda-12@users.noreply.github.com>
2021-08-23 11:58:48 -0400

    Add files via upload

---
+++ integration/integration/turtlebot_scripts/README.md
@@ -0,0 +1,5 @@
+# turtlebot_scripts
+scripts for creating nodes to communicate with the turtlebots
+
+see attached files in the google drive for instructions on setup and installation.
+https://drive.google.com/drive/folders/1UfbqRkns93QGAC8LzG_3oDZarix0j_va
---
+++ integration/integration/turtlebot_scripts/battery_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# have to define variable here to prevent error with ros
+batteryLevel = 100
+import integration.integration.turtlebot_scripts.message_creator as message_creator
+
+def battery(data):
+    """
+    
+    Gets battery percentage from subscribed ROS topic and
+    converts it into a percentage out of 100. 
+    
+    """
+    global batteryLevel
+    message_creator.batteryLevel = batteryLevel
+
+    # this function was found empirically through multiple tests
+    # with a turtlebot. It is an estimate.
+    batteryLevel = 100*1.74*(data.percentage-0.986)/(1.225-0.986)
