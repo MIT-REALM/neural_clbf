604c278dd4b793331b1f45dea59ae4ad003aa86a
Charles Dawson <cbd@mit.edu>
2021-08-24 15:51:38 -0400

    Investigating bug in sat unsafe region

--- neural_clbf/training/train_linear_satellite.py
+++ neural_clbf/training/train_linear_satellite.py
@@ -122,7 +122,7 @@ def main(args):
         cbf_lambda=1.0,
         controller_period=controller_period,
         cbf_relaxation_penalty=1e2,
-        scale_parameter=1.0,
+        scale_parameter=10.0,
     )
 
     # Initialize the logger and trainer
