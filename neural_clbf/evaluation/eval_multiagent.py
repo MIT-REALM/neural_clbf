671dbb070bd4b27ff489198faff645ec077375fc
Bethany Lowenkamp <bethlow@mit.edu>
2022-03-20 23:29:50 -0400

    evaluation multiagent outline

---
+++ neural_clbf/evaluation/eval_multiagent.py
@@ -0,0 +1,38 @@
+""""""
+import pandas as pd
+
+from neural_clbf.controllers import NeuralCLBFController
+
+def eval_multiagent():
+    # Load the checkpoint file. This should include the experiment suite used during
+    # training.
+    log_dir = "saved_models/multiagent/commit_63ca7c9/"
+    neural_controller = NeuralCLBFController.load_from_checkpoint(
+        log_dir + "version_0.ckpt"
+    )
+    # Run the experiments and save the results
+    neural_controller.experiment_suite.run_all_and_save_to_csv(
+        neural_controller, log_dir + "experiments"
+    )
+
+def plot_multiagent():
+    # Load the checkpoint file. This should include the experiment suite used during
+    # training.
+    log_dir = "saved_models/multiagent/commit_63ca7c9/"
+    neural_controller = NeuralCLBFController.load_from_checkpoint(
+        log_dir + "version_0.ckpt"
+    )
+
+    # Set the path to load from
+    experiment_dir = log_dir + "experiments/2021-03-13_11_11_33/"
+
+    # Rollout State Space Experiment
+    results_df = pd.read_csv(experiment_dir + "/Multiagent Experiment.csv")
+    neural_controller.experiment_suite.experiments[1].plot(
+        neural_controller, results_df, display_plots=True
+    )
+
+
+if __name__ == "__main__":
+    # eval_multiagent()
+    plot_multiagent()
